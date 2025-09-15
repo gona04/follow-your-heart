9097faaed1577c33426f1fead5626d1d18d61b85
Work Goonja <gandhi.goonja@gmail.com>
2025-09-15 11:37:26 +0530

    Cleaned up more code

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import carouselReducer from './carouselSlice';
 import typingReducer from './typingSlice';
 
 const store = configureStore({
   reducer: {
-    carousel: carouselReducer,
     typing: typingReducer,
   },
 });
